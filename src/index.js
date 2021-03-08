3a48565f8189cfd90bcebd0c416d375e170c551e
RobinN <robinnavas@gmail.com>
2021-03-08 11:12:12 +0100

    creation of the project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <div>Welcome to my-webpack-react-starter</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ webpack.common.js
@@ -0,0 +1,64 @@
+//  Les imports relatifs à webpack
+const path = require("path");
+
+//  Les plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+//  On définit les dossiers à exclure
+const nodeFolder = path.resolve(__dirname, "node_modules");
+// if we use yarn
+// const yarnModules = path.resolve(__dirname, '')
+module.exports = {
+  entry: "./src",
+
+  output: {
+    filename: "[name].bundle.js",
+
+    path: path.resolve(__dirname, "dist"),
+
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: nodeFolder,
+        use: "babel-loader",
+        enforce: "pre"
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: nodeFolder,
+        use: [
+          "MiniCssExtractPlugin.loader",
+          "css-loader",
+          "sass-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env", {}]]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource"
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Template Youhou",
+      template: "src/page/template.ejs"
+    }),
+    new MiniCssExtractPlugin()
+  ],
+
+  resolve: {}
+};
