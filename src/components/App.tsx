dfd7064f97c4d944a1ea19a2e0242cb26342ce43
RobinNavas <robinnavas@gmail.com>
2021-04-13 15:02:53 +0200

    Remove the default component and replace it by App component

---
+++ src/components/App.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+interface MyProps {
+
+}
+
+import '../styles/main.scss';
+
+export default class App extends React.Component<MyProps>{
+    constructor(props: MyProps) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                aaa
+            </div>
+        )
+    }
+
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return (
-    <div>
-      <div>Welcome to my-webpack-react-starter</div>
-    </div>
-  );
-};
-
-import TestComponent from "./components/CompoName";
+import App from "./components/App";
 
 ReactDOM.render(
-  <div>
-    <App />
-    <TestComponent firstProp={15}/> 
-  </div>,
-  document.querySelector("#root")
+    <div>
+        <App />
+    </div>,
+    document.querySelector("#root")
 );
