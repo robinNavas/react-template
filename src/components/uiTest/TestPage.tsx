bfa077b4108d5ee3284becb633b415d6a42f3f06
RobinNavas <robinnavas@gmail.com>
2021-04-13 17:01:01 +0200

    create our ui visual test component

---
+++ src/components/uiTest/TestPage.tsx
@@ -0,0 +1,25 @@
+
+// We use this Page to test each kind of ui components and html elements
+
+import React from 'react';
+
+// Import Every Components
+
+interface MyProps {
+
+}
+
+export default class TestPage extends React.Component<MyProps>{
+    constructor(props: MyProps) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
