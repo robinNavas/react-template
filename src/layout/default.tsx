12a8178c35427b9fc1e9855e5b64351e0c6afea3
RobinNavas <robinnavas@gmail.com>
2021-04-19 15:49:10 +0200

    create default components

---
+++ src/layout/default.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+interface MyProps {
+
+}
+
+export default class DefaultLayout extends React.Component<MyProps>{
+    constructor(props: MyProps) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
